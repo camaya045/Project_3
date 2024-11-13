9afbd9c6d416d0971cdb85c2a7f51c9b635f2d62
camaya045 <camaya045@gmail.com>
2024-11-12 23:12:24 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,42 @@
-// map.js
+let map;
+let marker;
+let infoWindow;
+
 function initMap() {
-  const mapOptions = {
-    center: { lat: 41.8781, lng: -87.6298 }, // Coordinates for Chicago, as an example
-    zoom: 10,
-  };
-  const map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  // Set the initial center of the map
+  const center = { lat: 44.429, lng: -110.584 };
+
+  // Initialize the map
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: center,
+    zoom: 8,
+  });
+
+  // Create the initial marker
+  marker = new google.maps.Marker({
+    position: center,
+    map: map,
+    title: "This is a custom marker",
+  });
+
+  // Create an info window for the marker
+  infoWindow = new google.maps.InfoWindow({
+    content: "<h3>Custom Marker</h3><p>This is an example of a custom marker with an info window.</p>",
+  });
+
+  // Add a click event to open the info window when the marker is clicked
+  marker.addListener("click", () => {
+    infoWindow.open(map, marker);
+  });
+
+  // Add a click event on the map to create new markers at clicked locations
+  map.addListener("click", function (event) {
+    const clickedLocation = event.latLng;
+
+    new google.maps.Marker({
+      position: clickedLocation,
+      map: map,
+      title: "New Marker",
+    });
+  });
 }
